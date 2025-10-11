2c5b6fb2d5d560232e433d5b7a93b58a3a36c698
ST10476635 <ST10476635@imconnect.edu.za>
2025-10-11 15:13:50 +0200

    Add imports

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 
 export default function App() {
   return (
